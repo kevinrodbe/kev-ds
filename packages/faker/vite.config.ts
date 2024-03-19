6873962f68518ba8be75a6c449c1fa7b290f3f86
Kevin RodBe <kevin.rodriguez992@gmail.com>
2024-03-19 12:25:36 -0500

    feat: add + config vite

---
+++ packages/faker/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/packages/themes',
+	build: {
+		outDir: '../../dist/packages/themes',
+		reportCompressedSize: true,
+		cssCodeSplit: false,
+		cssMinify: 'esbuild',
+		minify: 'esbuild',
+		lib: {
+			entry: 'src/index.ts',
+			name: 'themes',
+			fileName: 'index',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			output: {
+				assetFileNames: 'myTheme[extname]',
+			},
+			// External packages that should not be bundled into your library.
+			external: [],
+		},
+	},
+});
